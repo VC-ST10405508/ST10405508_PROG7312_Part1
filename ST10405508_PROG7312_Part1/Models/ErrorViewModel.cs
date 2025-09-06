e54a3f4aa30597b74d96cb554ec6f4a1b2dcf62e
ST10405508 - Joshua ponquett <ST10405508@vcconnect.edu.za>
2025-09-06 10:34:10 +0200

    Init project - MVC

---
+++ ST10405508_PROG7312_Part1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10405508_PROG7312_Part1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
